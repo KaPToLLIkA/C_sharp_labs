5976a49809aea8752cde6820d3f6b1917a2001b2
purdenko_konstantine <eduardpur18@gmail.com>
2020-07-03 23:26:25 +0300

    added service

---
+++ LAB5_SERVICE_UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LAB5_SERVICE_UI.SpellService1;
+
+namespace LAB5_SERVICE_UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            SpellServiceSoapClient client = new SpellServiceSoapClient();
+            SpellError[] err = client.checkText("", "ru", 0, "plain");
+
+
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
